8f8ec9815ad63a314f85278f92592f5f08e3abad
Neil Dahlke <neil@dahlke.io>
2020-05-04 18:49:46 -0700

    Bump PyPi version.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = '2020, Neil Dahlke'
 author = 'Neil Dahlke'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
