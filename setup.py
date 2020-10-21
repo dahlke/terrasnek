2d5877dd0b7a066632764e17e49b9e6a001fa96a
Neil Dahlke <neil.dahlke@gmail.com>
2020-10-20 17:37:34 -0700

    Fix the PyPi setup file.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "requests==2.21.0"
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
