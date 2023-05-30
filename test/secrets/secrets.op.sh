ba0b1f9d4a0a1b19ef433a116440125e5b51c8e7
Neil Dahlke <neil.dahlke@gmail.com>
2023-05-29 17:48:36 -0700

    Updating completeness and secrets file for one password.

--- test/secrets/secrets.op.sh
+++ test/secrets/secrets.op.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-eval $(op signin my)
+eval $(op signin)
 
 # Terraform Cloud/Enterprise Creds
 export TFC_URL="https://app.terraform.io"
-export TFC_TOKEN=$(op get item "Terraform Cloud" | jq -r '.details.sections[1].fields[0].v')
-export TFC_ORG_TOKEN=$(op get item "Terraform Cloud" | jq -r '.details.sections[1].fields[1].v')
+
+export TFC_TOKEN=$(op item get "Terraform Cloud" --format=json | jq -r '.fields[3].value')
+export TFC_ORG_TOKEN=$(op item get "Terraform Cloud" --format=json | jq -r '.fields[4].value')
 
 # SSL config
 export SSL_VERIFY="true"
 
 # AWS Credentials
-export AWS_ACCESS_KEY_ID=$(op get item Amazon | jq -r '.details.sections[1].fields[0].v')
-export AWS_SECRET_ACCESS_KEY=$(op get item Amazon | jq -r '.details.sections[1].fields[1].v')
+export AWS_ACCESS_KEY_ID=$(op item get Amazon --format=json | jq -r '.fields[3].value')
+export AWS_SECRET_ACCESS_KEY=$(op item get Amazon --format=json | jq -r '.fields[4].value')
 
 # GitHub Secrets for VCS OAuth
-export GITHUB_TOKEN=$(op get item GitHub | jq -r '.details.sections[1].fields[0].v')
-export GITHUB_SECRET=$(op get item GitHub | jq -r '.details.sections[1].fields[1].v')
+export GITHUB_TOKEN=$(op item get GitHub --format=json | jq -r '.fields[3].value')
+export GITHUB_SECRET=$(op item get GitHub --format=json | jq -r '.fields[4].value')
 
 # Twilio Secrets for Admin Settings Testing
-export TWILIO_ACCOUNT_SID=$(op get item Twilio | jq -r '.details.sections[1].fields[0].v')
-export TWILIO_AUTH_TOKEN=$(op get item Twilio | jq -r '.details.sections[1].fields[1].v')
+export TWILIO_ACCOUNT_SID=$(op item get Twilio --format=json | jq -r '.fields[3].value')
+export TWILIO_AUTH_TOKEN=$(op item get Twilio --format=json | jq -r '.fields[4].value')
 
 # SMTP Secrets for Admin Settings Testing
-export SMTP_HOST=$(op get item Sendgrid | jq -r '.details.sections[1].fields[0].v')
-export SMTP_PORT=$(op get item Sendgrid | jq -r '.details.sections[1].fields[1].v')
-export SMTP_USERNAME=$(op get item Sendgrid | jq -r '.details.sections[1].fields[2].v')
-export SMTP_PASSWORD=$(op get item Sendgrid | jq -r '.details.sections[1].fields[3].v')
+export SMTP_HOST=$(op item get Sendgrid --format=json | jq -r '.fields[3].value')
+export SMTP_PORT=$(op item get Sendgrid --format=json | jq -r '.fields[4].value')
+export SMTP_USERNAME=$(op item get Sendgrid --format=json | jq -r '.fields[5].value')
+export SMTP_PASSWORD=$(op item get Sendgrid --format=json | jq -r '.fields[6].value')
 
 # Constants used for creation and testing
 export TEST_EMAIL="neil.dahlke+terrasnek_unittest@gmail.com"
@@ -37,4 +38,4 @@ export TEST_TEAM_NAME="terrasnek-unittest"
 export TEST_PASSWORD=""
 
 # CodeCov Credentials
-export CODECOV_TOKEN=$(op get item CodeCov | jq -r '.details.sections[1].fields[0].v')
+export CODECOV_TOKEN=$(op item get CodeCov --format=json | jq -r '.fields[3].value')
